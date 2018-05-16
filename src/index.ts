3a6ba339ba5bc5d170ef8364a93cd0d60cecf443
juan@owsas.com <juan@owsas.com>
2018-05-16 15:44:28 -0500

    tested basic operations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,60 @@
-export class MyModule {
-  
+import assert from './assert';
+
+export interface IGetURLParams {
+  operation: 'cover'|'width'|'height'|'cdn';
+  asset?: boolean;
+  width?: number;
+  height?: number;
+  grayscale?: boolean;
+  blur?: number;
+  sharpen?: number;
+  trim?: number;
+  linear?: string;
+}
+
+export class ImageBoss {
+  public static assetHost: string;
+
+  /**
+   * Converts your original url to an url that uses Imageboss' services
+   * @param originalURL
+   * @param params 
+   */
+  static getURL(originalURL:string, params: IGetURLParams): string {
+    if (!params.operation) {
+      throw new Error('please set an operation to perform');
+    }
+
+    // get a clean url
+    let url: string = `https://img.imageboss.me/${params.operation}`;
+    const shouldHaveFilters: boolean = params.operation !== 'cdn';
+
+    // Process the operation
+    if (params.operation === 'cover') {
+      assert(params.width && params.height, 'cover operation requires width and height');
+      url = `${url}/${params.width}x${params.height}`;
+    } else if (params.operation === 'width') {
+      assert(params.width, 'width operation requires the width as a parameter');
+      url = `${url}/${params.width}`;        
+    } else if (params.operation === 'height') {
+      assert(params.height, 'height operation requires the height as a parameter');
+      url = `${url}/${params.height}`;        
+    }
+
+    // Add filters
+    if (shouldHaveFilters) {
+      if (params.grayscale) {
+        url = `${url}/grayscale:${params.grayscale}`;
+      } else if (params.blur) {
+        url = `${url}/blur:${params.blur}`;
+      } else if (params.sharpen) {
+        url = `${url}/sharpen:${params.sharpen}`;
+      } else if (params.trim) {
+        url = `${url}/trim:${params.trim}`;
+      }
+    }
+
+    return `${url}/${params.asset ? ImageBoss.assetHost + '/' : ''}${originalURL}`;
+  }
+
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,36 @@
-import { MyModule } from '../src/index';
+import { ImageBoss } from '../src';
 
-test('test', () => {
-  expect(true).toBe(true);
+describe('#getURL', () => {
+  describe('cover operation', () => {
+    test('should return the expected url', () => {
+      const url = ImageBoss.getURL(
+        'https://example.com/test.img', 
+        { operation: 'cover', width: 300, height: 200 },
+      );
+      
+      expect(url).toEqual('https://img.imageboss.me/cover/300x200/https://example.com/test.img');
+    });
+  });
+
+  describe('width operation', () => {
+    test('should return the expected url', () => {
+      const url = ImageBoss.getURL(
+        'https://example.com/test.img', 
+        { operation: 'width', width: 300 },
+      );
+      
+      expect(url).toEqual('https://img.imageboss.me/width/300/https://example.com/test.img');
+    });
+  });
+
+  describe('height operation', () => {
+    test('should return the expected url', () => {
+      const url = ImageBoss.getURL(
+        'https://example.com/test.img', 
+        { operation: 'width', height: 500 },
+      );
+      
+      expect(url).toEqual('https://img.imageboss.me/height/500/https://example.com/test.img');
+    });
+  });
 });
